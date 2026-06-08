871944a484f6c82b754952eb0555d98272204ecb
unknown <nerygutierritoz@gmail.com>
2026-06-08 18:51:51 -0500

    feat:add backend

---
+++ db/setup.sql
@@ -0,0 +1,16 @@
+-- Active: 1780953484038@@127.0.0.1@5432@animals_db
+-- Active: 1780953484038@@127.0.0.1@5432@animals_db
+CREATE TABLE animals (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  species VARCHAR(100) NOT NULL,
+  age INTEGER,
+  habitat VARCHAR(100),
+  endangered BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO animals (name, species, age, habitat, endangered) VALUES
+  ('Leo', 'León', 8, 'Sabana', false),
+  ('Dumbo', 'Elefante africano', 12, 'Sabana', true),
+  ('Nemo', 'Pez payaso', 2, 'Arrecife de coral', false),
+  ('Baloo', 'Oso pardo', 15, 'Bosque templado', false);
