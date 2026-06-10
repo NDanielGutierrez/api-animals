8bf9640ee4534d82e81f25c87317e4afea218078
unknown <nerygutierritoz@gmail.com>
2026-06-10 10:51:23 -0500

    fix:se corrigio .env

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { loadEnvFile } = require('node:process');
 const express = require('express');
 const animalsRouter = require('./routes/animals');
 
-loadEnvFile('.env');
+
+if (process.env.NODE_ENV !== 'production') {loadEnvFile('.env');}
 const app = express();
 const PORT = process.env.PORT || 3000;
 
